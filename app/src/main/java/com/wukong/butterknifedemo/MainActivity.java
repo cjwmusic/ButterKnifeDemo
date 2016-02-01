dba817335d9ef7b63f48cdb6052a610ceb7d91f4
wukong <caijunwei1990@163.com>
2016-02-02 00:38:43 +0800

    ButterKnife的使用。

--- app/src/main/java/com/wukong/butterknifedemo/MainActivity.java
+++ app/src/main/java/com/wukong/butterknifedemo/MainActivity.java
@@ -2,12 +2,23 @@ package com.wukong.butterknifedemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.MainTextView)
+    TextView mainTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mainTextView.setText("悟空来了~");
     }
 }
